8c54e45db044b9048c8bc598f1e26387ed6ad945
Ben Haggard <21bhaggard@gmail.com>
2025-01-23 18:23:46 -0700

    final commit

---
+++ FoodItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission3
+{
+    public class FoodItem
+    {
+        // when using {get; set;} like this C# will create a private field for you behind the scenes
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public HashSet<string> Categories { get; set; }
+        public DateTime ExpirationDate { get; set; }
+        // constructor is public to allow other classes to create FoodItem objects
+        public FoodItem(string name, int quantity, HashSet<string> categories, DateTime expirationDate)
+        {
+            Name = name;
+            Quantity = quantity;
+            Categories = categories;
+            ExpirationDate = expirationDate;
+        }
+
+        // this overrides the default ToString() method (inherited from the Object class) and defines how the object is printed
+        public override string ToString()
+        {
+            return $"Name: {Name}, Quantity: {Quantity}, Categories: {string.Join(", ", Categories)}, Expiration Date: {ExpirationDate}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,52 +1,72 @@
-﻿// food bank inventory
-public class FoodItem
+﻿using Mission3;
+internal class Program
 {
-    // constructor
-    FoodItem(string name, int quantity, HashSet<string> categories, DateTime expirationDate)
+    private static void Main(string[] args)
     {
-        Name = name;
-        Quantity = quantity;
-        Categories = categories;
-        ExpirationDate = expirationDate;
-    }
-
-    // when using {get; set;} like this C# will create a private field for you behind the scenes
-    public string Name { get; set; }
-    public int Quantity { get; set; }
-    public HashSet<string> Categories { get; set; }
-    public DateTime ExpirationDate { get; set; }
-
-    // this overrides the default ToString() method (inherited from the Object class) and defines how the object is printed
-    public override string ToString()
-    {
-        return $"Name: {Name}, Quantity: {Quantity}, Categories: {string.Join(", ", Categories)}, Expiration Date: {ExpirationDate}";
-    }
-}
-
-// use static class to store global state (inventory)
-static class Inventory 
-{
-    // static classes do not have constructors
-    private static List<FoodItem> _inventory = new List<FoodItem>();
-
-    // method to add item to inventory
-    public static void AddItem(FoodItem item)
-    {
-        _inventory.Add(item);
-    }
-
-    // method to remove item from inventory
-    public static void RemoveItem(FoodItem item)
-    {
-        _inventory.Remove(item);
-    }
-    
-    // method to print inventory
-    public static void PrintInventory()
-    {
-        foreach (var item in _inventory)
+        List<FoodItem> Inventory = new List<FoodItem>();
+        // main program logic
+        while (true)
         {
-            Console.WriteLine(item);
+            
+            Console.WriteLine("Inventory Management System");
+            Console.WriteLine("1. Add Item");
+            Console.WriteLine("2. Remove Item");
+            Console.WriteLine("3. Print Inventory");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("Choose an option: ");
+            string choice = Console.ReadLine();
+            if (choice != "1" && choice != "2" && choice != "3" && choice != "4")
+            {
+                Console.WriteLine("Invalid choice. Please try again.");
+            }
+            else
+            {
+                if (choice == "1")// add a FoodItem
+                {
+                    Console.WriteLine("Enter item name: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Enter item quantity: ");
+                    int quantity = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter item categories (comma separated): ");
+                    //create a new hash set with the categories, split using delimiter ',' and trim spaces
+                    HashSet<string> categories = new HashSet<string>(Console.ReadLine().Split(',').Select(c => c.Trim()));
+                    Console.WriteLine("Enter item expiration date (yyyy-mm-dd): ");
+                    // parse the date string into a DateTime object
+                    DateTime expirationDate = DateTime.Parse(Console.ReadLine());
+                    // create new FoodItem object and add it to the inventory
+                    FoodItem newItem = new(name, quantity, categories, expirationDate);
+                    Inventory.Add(newItem);
+                }
+                else if (choice == "2")// remove food item from inventory
+                {
+                    // find food item by name and remove
+                    Console.WriteLine("Enter item name to remove: ");
+                    string nameToRemove = Console.ReadLine();
+                    // .RemoveAll() will return the number of items removed, if any were removed it will be > 0
+                    int itemsRemoved = Inventory.RemoveAll(i => i.Name == nameToRemove);
+                    if (itemsRemoved != 0)
+                    {
+                        Console.WriteLine($"Removed all {nameToRemove} from inventory.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Item {nameToRemove} not found in inventory.");
+                    }
+                }
+                else if (choice == "3")
+                {
+                    foreach (var item in Inventory)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                else if (choice == "4")
+                {
+                    break;
+                }
+            }
         }
     }
 }
+
+
